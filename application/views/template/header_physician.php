597949c17492c28f616775393ae39bb46a46504f
wanalbino <wanwancsyy@gmail.com>
2021-02-18 15:25:56 +0700

    change logo Clinic dashboard

--- application/views/template/header_physician.php
+++ application/views/template/header_physician.php
@@ -39,7 +39,7 @@
 <body class="fixed-nav sticky-footer" id="page-top">
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-default fixed-top" id="mainNav">
-    <a class="navbar-brand" href="<?php echo base_url('physician')?>"><img src="<?php echo base_url() ?>assets/img/nutmor_logo.png" data-retina="true" alt="" width="163" height="36"></a>
+    <a class="navbar-brand" href="<?php echo base_url('physician')?>"><img src="<?php echo base_url() ?>assets/img/nutmor_logo_white.png" data-retina="true" alt="" width="163" height="36"></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
