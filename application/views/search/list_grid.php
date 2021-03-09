3a1cff65d90b7a0ba4564967ce4954d7841c0823
wanalbino <wanwancsyy@gmail.com>
2021-03-09 20:57:45 +0700

    fix : short if error

--- application/views/search/list_grid.php
+++ application/views/search/list_grid.php
@@ -73,10 +73,10 @@
                                 </a>
                             </figure>
                             <div class="wrapper">
-                                <small><?php echo $item->DETAIL ?? ''; ?></small>
-                                <h3><?php echo $item->DOCTORNAME ?? ''; ?></h3>
+                                <small><?php echo $item->DETAIL ? : ''; ?></small>
+                                <h3><?php echo $item->DOCTORNAME ? : ''; ?></h3>
 
-                                <p><?php echo $item->SERVICE ?? ''; ?></p>
+                                <p><?php echo $item->SERVICE ? : ''; ?></p>
                                 <span class="rating"><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star voted"></i><i class="icon_star"></i><i class="icon_star"></i> <small>(145)</small></span>
                                 <a href="badges.html" data-toggle="tooltip" data-placement="top" data-original-title="Badge Level" class="badge_list_1"><img src="img/badges/badge_1.svg" width="15" height="15" alt=""></a>
                             </div>
