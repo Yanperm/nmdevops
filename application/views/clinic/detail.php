a7864331325925b8123ea5330579828636e16f32
wanalbino <wanwancsyy@gmail.com>
2021-03-31 13:46:20 +0700

    Update : Detail Clinic address

--- application/views/clinic/detail.php
+++ application/views/clinic/detail.php
@@ -60,11 +60,11 @@
                                     <ul class="contacts">
                                         <li>
                                             <h6>ที่อยู่</h6>
-                                            <?php echo $clinic->PROVINCE; ?>
+                                            <?php echo $clinic->ADDRESS; ?>
                                             <a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@<?php echo $clinic->LAT; ?>,<?php echo $clinic->LONG; ?>,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank"> <strong>ดูบนแผนที่</strong></a>
                                         </li>
                                         <li>
-                                            <h6>Contact</h6> โทร:<a href="tel:<?php echo $clinic->PHONE; ?>"> <?php echo $clinic->PHONE; ?></a> Line: <a href="http://line.me/ti/p/<?php echo $clinic->LINE; ?>" target="_blank"> <?php echo $clinic->LINE; ?></a></li>
+                                            <h6>ติดต่อสอบถามการบริการ</h6> โทร:<a href="tel:<?php echo $clinic->PHONE; ?>"> <?php echo $clinic->PHONE; ?></a> Line: <a href="http://line.me/ti/p/<?php echo $clinic->LINE; ?>" target="_blank"> <?php echo $clinic->LINE; ?></a></li>
                                     </ul>
                                 </div>
                             </div>
