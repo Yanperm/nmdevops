a2dd8186c8f74fe7d12c5f5352476f4459680bf0
wanalbino <wanwancsyy@gmail.com>
2021-07-05 16:25:10 +0700

    Update : Form add queue Clinic

--- application/views/physician/list_queue_form.php
+++ application/views/physician/list_queue_form.php
@@ -1,31 +1,39 @@
 <div class="row">
-<div class="col-xl-6 col-lg-6 col-md-6">
-    <div class="list_time">
-        <div class="list_title">
-            <i class="icon_clock_alt"></i>
-            <h5 style='color:#ffffff !important'><?php echo $clinic->CLINICNAME; ?> <?php echo $date; ?></h3>
-        </div>
-        <?php
+    <div class="col-xl-6 col-lg-6 col-md-6">
+        <div class="list_time">
+            <div class="list_title">
+                <i class="icon_clock_alt"></i>
+                <h5 style='color:#ffffff !important'>
+                    <?php echo $clinic->CLINICNAME; ?>
+                    <?php echo $date; ?>
+                    </h3>
+            </div>
+            <?php
         $today = date($date);
         $number = date('w', strtotime($today));
         ?>
-        <?php if ($number == $clinic->DAYOFF || $closeStatus): ?>
-            <div class="alert alert-danger text-center" role="alert">
-                หยุดทำการ <?php echo $date; ?>
-            </div>
-        <?php endif; ?>
-        <?php if ($statusBooked): ?>
-            <div class="alert alert-info text-center" role="alert">
-                <p> ท่านได้ทำการนัดหมอในวันที่ <?php echo $date; ?></p>
-                <p>คิวที่จอง  : <?php echo $queueBooked?></p>
-                <p>เรียบร้อยแล้ว</p>
-            </div>
-        <?php endif; ?>
+                <?php if ($number == $clinic->DAYOFF || $closeStatus): ?>
+                <div class="alert alert-danger text-center" role="alert">
+                    หยุดทำการ
+                    <?php echo $date; ?>
+                </div>
+                <?php endif; ?>
+                <?php if ($statusBooked): ?>
+                <div class="alert alert-info text-center" role="alert">
+                    <p> ท่านได้ทำการนัดหมอในวันที่
+                        <?php echo $date; ?>
+                    </p>
+                    <p>คิวที่จอง :
+                        <?php echo $queueBooked?>
+                    </p>
+                    <p>เรียบร้อยแล้ว</p>
+                </div>
+                <?php endif; ?>
 
 
-        <?php if ($number != $clinic->DAYOFF && !$statusBooked && !$closeStatus): ?>
-            <ul>
-                <?php foreach ($times as $key => $time):
+                <?php if ($number != $clinic->DAYOFF && !$statusBooked && !$closeStatus): ?>
+                <ul>
+                    <?php foreach ($times as $key => $time):
                     $textTime = $time->format('H:i') . '-' . $time->add($interval)->format('H:i');
                     ?>
                     <li><strong><?php echo $textTime; ?></strong>
@@ -38,117 +46,179 @@
                             endif;
                         endforeach; ?>
 
-                        <?php if ($booked): ?>
+                            <?php if ($booked): ?>
                             <button class="booked" disabled>จองแล้ว</button>
-                        <?php endif; ?>
+                            <?php endif; ?>
 
-                        <?php if (!$booked): ?>
+                            <?php if (!$booked): ?>
                             <a href="javascript:void(0)" onclick="queue('<?=$textTime?>','A<?=($key + 1)?>','<?=($key + 1)?>')">จองคิว</a>
-                        <?php endif; ?>
+                            <?php endif; ?>
 
                     </li>
-                <?php endforeach; ?>
-                <!--                                --><?php //foreach ($bookingExtraQues as $key => $item):?>
-                <!--                                    <li style="background: #e9e6ee;"><strong style="width: 105px;">คิวเสริม</strong>-->
-                <!--                                        <span>B--><?php //echo $key + 1;?><!-- </span>-->
-                <!--                                        <button class="booked" disabled>จองแล้ว</button>-->
-                <!--                                    </li>-->
-                <!--                                --><?php //endforeach;?>
-                <li style="background: #e9e6ee;"><strong style="width: 105px;">คิวเสริม</strong>
-                    <span>B<?php echo count($bookingExtraQues) + 1; ?> </span>
-                    <a  href="javascript:void(0)" onclick="queue('รอเรียกจากเคาเตอร์','B<?=(count($bookingExtraQues) + 1)?>','<?=(50+count($bookingExtraQues) + 1)?>')" >จองคิว</a>
-                </li>
-                <?php if ($bookingExtraQuesC != null):?>
-                <li style="background: #e9e6ee;"><strong style="width: 120px;">ฉีดยา/ทำแผล</strong>
-                    <span>C<?php echo count($bookingExtraQuesC) + 1; ?> </span>
-                    <a href="javascript:void(0)" onclick="queue('รอเรียกจากเคาเตอร์','C<?=(count($bookingExtraQuesC) + 1)?>','<?=(101+count($bookingExtraQuesC) + 1)?>')">จองคิว</a>
-                </li>
-              <?php endif;?>
-            </ul>
-        <?php endif; ?>
+                    <?php endforeach; ?>
+                    <!--                                -->
+                    <?php //foreach ($bookingExtraQues as $key => $item):?>
+                    <!--                                    <li style="background: #e9e6ee;"><strong style="width: 105px;">คิวเสริม</strong>-->
+                    <!--                                        <span>B-->
+                    <?php //echo $key + 1;?>
+                    <!-- </span>-->
+                    <!--                                        <button class="booked" disabled>จองแล้ว</button>-->
+                    <!--                                    </li>-->
+                    <!--                                -->
+                    <?php //endforeach;?>
+                    <li style="background: #e9e6ee;"><strong style="width: 105px;">คิวเสริม</strong>
+                        <span>B<?php echo count($bookingExtraQues) + 1; ?> </span>
+                        <a href="javascript:void(0)" onclick="queue('รอเรียกจากเคาเตอร์','B<?=(count($bookingExtraQues) + 1)?>','<?=(50+count($bookingExtraQues) + 1)?>')">จองคิว</a>
+                    </li>
+                    <?php if ($bookingExtraQuesC != null):?>
+                    <li style="background: #e9e6ee;"><strong style="width: 120px;">ฉีดยา/ทำแผล</strong>
+                        <span>C<?php echo count($bookingExtraQuesC) + 1; ?> </span>
+                        <a href="javascript:void(0)" onclick="queue('รอเรียกจากเคาเตอร์','C<?=(count($bookingExtraQuesC) + 1)?>','<?=(101+count($bookingExtraQuesC) + 1)?>')">จองคิว</a>
+                    </li>
+                    <?php endif;?>
+                </ul>
+                <?php endif; ?>
+        </div>
     </div>
-</div>
-<div class="col-md-6">
-  <form action="<?php echo base_url('physician/insertQueue'); ?>" method="post" id="add-queue-form">
+    <div class="col-md-6">
+        <form action="<?php echo base_url('physician/insertQueue'); ?>" method="post" id="add-queue-form">
 
-    <input type="hidden" value="<?php echo $this->session->userdata('id'); ?>" name="clinic_id">
+            <input type="hidden" value="<?php echo $this->session->userdata('id'); ?>" name="clinic_id">
 
-    <h4>รายละเอียด</h4>
-    <table style='font-size: 20px;'>
-      <tr>
-        <td>วันที่</td>
+            <h4>รายละเอียด</h4>
+            <table style='font-size: 20px;'>
+                <tr>
+                    <td>วันที่</td>
 
-        <td class="text_queue"><?php echo $date; ?></td>
-      </tr>
-      <tr>
-        <td>เวลา</td>
-        <td class="text_queue" id='text-time'></td>
-      </tr>
-      <tr>
-        <td>คิวที่</td>
-        <td class="text_queue" id='text-queue'></td>
-      </tr>
-    </table>
-    <hr>
-    <div id="form-queue" style='display:none'>
-    <p>กรุณากรอกข้อมูลคนไข้</p>
-    <input type='hidden' id="time" name='time'>
-    <input type='hidden' id="date" name='date' value="<?php echo $date; ?>">
-    <input type='hidden' id="queue" name='queue'>
-    <input type='hidden' id="qber" name='qber'>
-    <div class="form-group">
-        <label>เมลล์</label>
-        <input type="email" class="form-control" placeholder="" id="email" name="email" required onBlur="checkEmail(this.value)">
-    </div>
-    <p id="error-email" style="display:none;color: #f44336;">เมลล์นี้มีการใช้งานในระบบ</p>
-    <div class="form-group">
-        <label>ชื่อ</label>
-        <input type="text" class="form-control" placeholder="" name="firstname_booking" required>
-    </div>
-    <div class="form-group">
-        <label>นามสกุล</label>
-        <input type="text" class="form-control" placeholder="" name="lastname_booking" required>
-    </div>
-    <div class="form-group">
-        <label>เบอร์โทรศัพท์ติดต่อ</label>
-        <input type="text" class="form-control" placeholder="" name="telephone" required>
-    </div>
-    <div class="form-group">
-        <label>Line Id</label>
-        <input type="text" class="form-control" placeholder="" name="line_id" >
-    </div>
-   
-    <div class="form-group">
-        <label>สาเหตุที่มาพบแพทย์</label>
-        <textarea class="form-control" name="cause" required></textarea>
-    </div>
-    <div class="form-group">
-        <button class="btn btn-success" type="button" id="btn-add" onclick="addQueue()" disabled="disabled">ยืนยันการนัดหมอ</button>
-        <button class="btn btn-default"type="reset">ล้างข้อมูล</button>
-    </div>
+                    <td class="text_queue">
+                        <?php echo $date; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td>เวลา</td>
+                    <td class="text_queue" id='text-time'></td>
+                </tr>
+                <tr>
+                    <td>คิวที่</td>
+                    <td class="text_queue" id='text-queue'></td>
+                </tr>
+            </table>
+            <hr>
+            <div id="form-queue" style='display:none'>
+                <p>กรุณากรอกข้อมูลคนไข้</p>
+                <input type='hidden' id="time" name='time'>
+                <input type='hidden' id="date" name='date' value="<?php echo $date; ?>">
+                <input type='hidden' id="queue" name='queue'>
+                <input type='hidden' id="qber" name='qber'>
+                <div class="form-group">
+                    <label>เมลล์</label>
+                    <input type="email" class="form-control" placeholder="" id="email" name="email" required onBlur="checkEmail(this.value)">
+                </div>
+                <div>
+
+                </div>
+                <p id="error-email" style="display:none;color: #f44336;">เมลล์นี้มีการใช้งานในระบบ</p>
+                <div class="form-group">
+                    <label>ชื่อ</label>
+                    <input type="text" class="form-control" placeholder="" id="fname" name="firstname_booking" required>
+                </div>
+                <div class="form-group">
+                    <label>นามสกุล</label>
+                    <input type="text" class="form-control" placeholder="" id="lname" name="lastname_booking" required>
+                </div>
+                <div class="form-group">
+                    <label>เบอร์โทรศัพท์ติดต่อ</label>
+                    <input type="text" class="form-control" placeholder="" id="telephone" name="telephone" required>
+                </div>
+                <div class="form-group">
+                    <label>Line Id</label>
+                    <input type="text" class="form-control" placeholder="" id="line_id" name="line_id">
+                </div>
+
+                <div class="form-group">
+                    <label>สาเหตุที่มาพบแพทย์</label>
+                    <textarea class="form-control" name="cause" required></textarea>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success" type="button" onclick="addQueue()">ยืนยันการนัดหมอ</button>
+                    <button class="btn btn-default" type="button" onclick="resetData()">ล้างข้อมูล</button>
+                </div>
 
 
-  </form>
-</div>
-</div>
+        </form>
+        </div>
+    </div>
 </div>
 <script>
-function checkEmail(email){
-    $.ajax({
-     url: '<?php echo base_url('physician/checkEmail')?>',
-     type: 'get',
-     data : {email : email},
-     success: function(response) {
-      let data  = JSON.parse(response);
-      
-      if(Object.keys(data.member).length === 0 ){
-        $('#error-email').hide();
-        $("#btn-add").prop("disabled", false);
-      }else{
-        $('#error-email').show();
-        $("#btn-add").prop("disabled", true);
-      }
-     }
-   });
-}
+    function checkEmail(email) {
+
+        $.ajax({
+            url: "<?php echo base_url('physician/checkEmail');?>",
+            type: 'get',
+            data: {
+                email: email
+            },
+            success: function(response) {
+                let data = JSON.parse(response);
+
+                if (Object.keys(data.member).length === 0) {
+                    $("#fname").val('');
+                    $("#lname").val('');
+                    $("#telephone").val('');
+                    $("#line_id").val('');
+
+                    $("#fname").removeAttr('disabled');
+                    $("#lname").removeAttr('disabled');
+                    $("#telephone").removeAttr('disabled');
+                    $("#line_id").removeAttr('disabled');
+                } else {
+
+                    Swal.fire({
+                        title: 'เมลล์นี้มีการใช้งาน!',
+                        text: "บัญชีคุณ " + data.member.CUSTOMERNAME + " ใช่หรือไม่",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'ใช่',
+                        cancelButtonText: 'ไม่ใช่'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            let name = data.member.CUSTOMERNAME.split(" ");
+
+                            $("#fname").val(name[0]);
+                            $("#lname").val(name[1]);
+                            $("#telephone").val(data.member.PHONE);
+                            $("#line_id").val(data.member.LINEID);
+
+                            $("#fname").prop("disabled", true);
+                            $("#lname").prop("disabled", true);
+                            $("#telephone").prop("disabled", true);
+                            $("#line_id").prop("disabled", true);
+                        } else {
+                            $('#email').val('');
+                            $("#fname").removeAttr('disabled');
+                            $("#lname").removeAttr('disabled');
+                            $("#telephone").removeAttr('disabled');
+                            $("#line_id").removeAttr('disabled');
+                        }
+                    })
+
+                }
+            }
+        });
+    }
+
+    function resetData() {
+        $("#email").val('');
+        $("#fname").val('');
+        $("#lname").val('');
+        $("#telephone").val('');
+        $("#line_id").val('');
+
+        $("#fname").removeAttr('disabled');
+        $("#lname").removeAttr('disabled');
+        $("#telephone").removeAttr('disabled');
+        $("#line_id").removeAttr('disabled');
+    }
 </script>
